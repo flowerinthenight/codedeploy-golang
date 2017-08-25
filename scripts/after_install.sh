b870621edfb48968991073b15252db06064bdc71
flowerinthenight <chew.esmero@gmail.com>
2017-08-25 11:24:08 +0900

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 # set golang path
-echo "export PATH=$PATH:/home/ubuntu/go/bin" >> $HOME/.bashrc
+# echo "export PATH=$PATH:/home/ubuntu/go/bin" >> $HOME/.bashrc
 
 # make golang workspace
-cd $HOME
-mkdir gocode
-echo "export GOPATH=$HOME/gocode" >> $HOME/.bashrc
-echo "export PATH=$PATH:$GOPATH/bin" >> $HOME/.bashrc
+# cd $HOME
+# mkdir gocode
+# echo "export GOPATH=$HOME/gocode" >> $HOME/.bashrc
+# echo "export PATH=$PATH:$GOPATH/bin" >> $HOME/.bashrc
 
 # activate changes
-source $HOME/.bashrc
+# source $HOME/.bashrc
 
 # install golang dependency management tool
-go get github.com/tools/godep
+# go get github.com/tools/godep
 
 # go to golang app
-cd $GOPATH/src/github.com/zemirco/codedeploy/
+cd $GOPATH/src/github.com/flowerinthenight/codedeploy-golang/
 
 # restore dependencies
-godep restore
+# godep restore
 
 # make binary
-go install
+go build -v
