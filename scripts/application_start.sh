1e9ee9d23978935cf2918ebeffdbf2f668753b22
flowerinthenight <chew.esmero@gmail.com>
2017-08-25 11:25:08 +0900

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 # usually sudo but not needed because appspec.yml has runas root
-service codedeploy start
+# service codedeploy start
+cd $GOPATH/src/github.com/flowerinthenight/codedeploy-golang/
+
+# restore dependencies
+# godep restore
+
+# make binary
+./codedeploy-golang
